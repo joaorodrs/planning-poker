1843041e54c7db0dd95b56c6d81461a4edcd6576
Joao Paulo Alencar Rodrigues <joaopauloalencarrodrigues@gmail.com>
2024-04-06 15:10:55 -0300

    feat: adapts mobile first styling to desktop

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import Header from './components/header'
 
 function App() {
   return (
-    <main className="w-screen h-screen">
+    <main className="w-screen h-screen flex flex-col justify-between">
       <Header />
 
-      <div className="px-10">
-        <div className="flex w-full justify-between">
+      <div className="px-10 w-full max-w-[550px] mx-auto">
+        <div className="flex justify-between">
           <div className="w-fit">
             <span className="text-gray-300">Junior</span>
             <div className="bg-primary-300 rounded-full size-10 mt-1" />
@@ -31,8 +31,10 @@ function App() {
           </div>
         </div>
 
-        <div className="bg-primary-300 h-24 w-full my-5 rounded-lg flex align-center justify-center">
-          <button className="bg-primary-600 h-fit px-5 py-2 text-sm rounded-lg m-auto font-medium">Revelar cartas</button>
+        <div className="bg-primary-300 h-24 w-full my-5 rounded-lg flex align-center justify-center md:h-40">
+          <button className="bg-primary-600 h-fit px-5 py-2 text-sm rounded-lg m-auto font-medium md:text-lg">
+            Revelar cartas
+          </button>
         </div>
 
         <div className="flex w-full justify-between">
