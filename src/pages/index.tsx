039cdf75bbdc57d54b79090555401e6c6f4efa73
Joao Paulo Alencar Rodrigues <joaopauloalencarrodrigues@gmail.com>
2024-04-13 17:15:55 -0300

    feat: adds user session management

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react'
-import { getFirestore } from 'firebase/firestore'
+import { FormEvent, useCallback, useEffect } from 'react'
+import { getFirestore, addDoc, query, collection, getDoc, getDocs, where, doc } from 'firebase/firestore'
 import { initializeApp } from 'firebase/app'
+import { redirect, useNavigate } from 'react-router-dom'
 import 'firebase/auth'
 import 'firebase/firestore'
 
-import Deck from '@/components/deck'
-import Header from '@/components/header'
-import PokerDesk from '@/components/poker-desk'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
-import SessionDialog from '@/components/dialogs/session-dialog'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+
 import '@/App.css'
 
 const app = initializeApp({
@@ -23,35 +24,81 @@ const app = initializeApp({
 
 const db = getFirestore(app);
 
+type FormValues = {
+  username: { value: string };
+}
+
 export default function Home() {
-  const [cardSelected, setCardSelected] = useState<number>()
-  const [open, setOpen] = useState(false);
+  const navigate = useNavigate()
 
-  function onSelectCard(value?: number) {
-    setCardSelected(value)
-  }
+  async function onSubmit(event: FormEvent) {
+    try {
+      event.preventDefault()
 
-  function getData() {
-    const sessionId = localStorage.getItem('@planning-poker:sessionId')
+      const values = event.target as unknown as FormValues;
 
-    if (!sessionId) {
-      setOpen(true)
+      const body = {
+        name: values.username?.value,
+      }
+
+      const doc = await addDoc(collection(db, "user"), body);
+
+      const data = await getDoc(doc)
+
+      const user = {
+        id: data.id,
+        name: data.data()?.name
+      }
+
+      localStorage.setItem('@planning-poker:user', JSON.stringify(user))
+
+      getData();
+    } catch (err) {
+      console.error(err)
     }
   }
 
+  const getData = useCallback(async () => {
+    const stringUser = localStorage.getItem('@planning-poker:user')
+
+    if (!stringUser) {
+      return
+    }
+
+    const user = JSON.parse(stringUser);
+
+    const docSnap = await getDoc(doc(db, "user", user.id))
+
+    if (docSnap.id) {
+      navigate('/session', { replace: false })
+    }
+  }, [navigate])
+
   useEffect(() => {
     getData()
-  }, [])
+  }, [getData])
 
   return (
     <main className="w-screen h-screen flex flex-col justify-between">
-      <Header />
-
-      <PokerDesk />
-
-      <Deck cardSelected={cardSelected} onSelectCard={onSelectCard} />
-
-      <SessionDialog isOpen={open} onClose={() => setOpen(false)} />
+      <Card className="border-none my-auto mx-4">
+        <form onSubmit={onSubmit}>
+          <CardHeader>
+            <CardTitle>Novo por aqui?</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Input name="username" placeholder="Qual o seu nome?" />
+          </CardContent>
+          <CardFooter>
+            <Button
+              className="bg-primary-400 text-black mt-1 w-full"
+              size="sm"
+              type="submit"
+            >
+              Enviar
+            </Button>
+          </CardFooter>
+        </form>
+      </Card>
     </main>
   )
 }
---
+++ src/pages/session.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import Deck from "@/components/deck"
+import Header from "@/components/header"
+import PokerDesk from "@/components/poker-desk"
+
+export default function Home() {
+  const [cardSelected, setCardSelected] = useState<number>()
+
+  function onSelectCard(value?: number) {
+    setCardSelected(value)
+  }
+
+  return (
+    <main className="w-screen h-screen flex flex-col justify-between">
+      <Header />
+
+      <PokerDesk />
+
+      <Deck cardSelected={cardSelected} onSelectCard={onSelectCard} />
+    </main>
+  )
+}
+
